16a35f02dd4901ed78df65c67d142d5f8cc282c0
Hang0706 <nhang1396@gmail.com>
2023-11-09 08:52:28 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+// Composables
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/admin',
+    component: () => import('../layouts/default/Default.vue'),
+    children: [
+      {
+        path: 'home',
+        component: () => import('../views/HomePage.vue'),
+      },
+    ],
+  },
+  {
+    path: '/',
+    component: () => import('../components/Login.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+
+
+export default router
