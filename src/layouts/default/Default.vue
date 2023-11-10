43f216b17aa3b8894a99568778473700ab0a7d99
Hang0706 <nhang1396@gmail.com>
2023-11-10 21:31:56 +0700

    message

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -210,6 +210,7 @@
               <ul role="list" class="-mx-2 space-y-1">
                 <li v-for="item in navigation" :key="item.name">
                   <a
+                    v-if="!item.children"
                     :href="item.href"
                     :class="[
                       item.current
@@ -230,6 +231,42 @@
                     />
                     {{ item.name }}
                   </a>
+                  <Disclosure as="div" v-else v-slot="{ open }">
+                    <DisclosureButton
+                      :class="[
+                        item.current ? 'bg-gray-50' : 'hover:bg-gray-50',
+                        'flex items-center w-full text-left rounded-md p-2 gap-x-3 text-sm leading-6 font-semibold text-gray-700',
+                      ]"
+                    >
+                      <component
+                        :is="item.icon"
+                        class="h-6 w-6 shrink-0 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      {{ item.name }}
+                      <ChevronRightIcon
+                        :class="[
+                          open ? 'rotate-90 text-gray-500' : 'text-gray-400',
+                          'ml-auto h-5 w-5 shrink-0',
+                        ]"
+                        aria-hidden="true"
+                      />
+                    </DisclosureButton>
+                    <DisclosurePanel as="ul" class="mt-1 px-2">
+                      <li v-for="subItem in item.children" :key="subItem.name">
+                        <!-- 44px -->
+                        <DisclosureButton
+                          as="a"
+                          :href="subItem.href"
+                          :class="[
+                            subItem.current ? 'bg-gray-50' : 'hover:bg-gray-50',
+                            'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700',
+                          ]"
+                          >{{ subItem.name }}</DisclosureButton
+                        >
+                      </li>
+                    </DisclosurePanel>
+                  </Disclosure>
                 </li>
               </ul>
             </li>
