6dd0f48933e8ffb9d6c309ed1a2d0e99684b1d16
minhng <minhng@hypersol.vn>
2023-12-14 00:13:08 +0700

    no message

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -26,9 +26,7 @@
           <div class="flex items-center justify-between">
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Mật khẩu
             </label>
-            <div class="text-sm">
-              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Quên mật khẩu ?</a>
-            </div>
+
           </div>
           <div class="mt-2">
             <input id="password" name="password" type="password" autocomplete="current-password"
