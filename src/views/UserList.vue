71f2b0bfb8f562d646bfea521222c9e88625e377
Hang0706 <nhang1396@gmail.com>
2023-12-19 12:35:28 +0700

    chi tiết khách hàng

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -142,7 +142,7 @@
             <dd
               class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"
             >
-              {{ selectedUser.customer.customerData.userId }}
+              {{ selectedUser.customer.customerData.id }}
             </dd>
           </div>
           <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-4">
